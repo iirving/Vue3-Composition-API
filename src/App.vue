95896e2d37ac76981943549fd764a4928a018a20
Ian Irving <ian@falsepositives.com>
2023-11-29 12:31:37 -0500

    Add shopping list functionality

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
 <script setup>
 import { ref } from 'vue'
 
-const msg = ref('Hello World!')
+const header = ref('Shopping List App')
+const items = ref([
+{id: 5, label: "10 party hats"},
+{id: 2, label: "2 board games"},
+{id: 3, label: "20 cups"}
+])
+let newItem = ref("");
+const newItemHighPriority = ref(false)
+const saveItem = () =>{
+  items.value.push({id: items.value.length+1, label: newItem.value});
+  newItem.value = "";
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <input v-model="msg">
+  <h1>{{ header || 'Welcome'}}</h1>
+  <form 
+    class="add-item-form"
+    @submit.prevent="saveItem" 
+  >
+  
+  <input type="text" 
+    placeholder="Add an item." 
+    v-model.trim="newItem">
+
+  <label>
+  <input type="checkbox" v-model="newItemHighPriority" value="high">
+  High Priority
+  </label>
+  <button 
+    class="btn btn-primary">
+  save
+  </button>
+  <br>
+  {{newItemHighPriority}}
+  </form>
+  <ul>
+    <li v-for="item in items" :key="item.id"> {{item.label}}</li>
+  </ul>
 </template>
